aae8b1d050016d155b32bf759af8f11d67a70cc0
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-08-28 13:03:16 +0500

    movies data updated

--- src/services/fakeMovieService.js
+++ src/services/fakeMovieService.js
@@ -1,5 +1,4 @@
 import * as genresAPI from "./fakeGenreService";
-
 const movies = [
   {
     _id: "5b21ca3eeb7f6fbccd471815",
@@ -63,6 +62,244 @@ const movies = [
     title: "The Avengers",
     genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Action" },
     numberInStock: 7,
+    dailyRentalRate: 3.5
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd471822",
+    title: "The Dark Knight",
+    genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Action" },
+    numberInStock: 8,
+    dailyRentalRate: 4.0
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd471823",
+    title: "Inception",
+    genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller" },
+    numberInStock: 6,
+    dailyRentalRate: 4.0
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd471824",
+    title: "Interstellar",
+    genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller" },
+    numberInStock: 5,
+    dailyRentalRate: 3.5
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd471825",
+    title: "Forrest Gump",
+    genre: { _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy" },
+    numberInStock: 7,
+    dailyRentalRate: 3.5
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd471826",
+    title: "Gladiator",
+    genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Action" },
+    numberInStock: 4,
+    dailyRentalRate: 3.0
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd471827",
+    title: "Shrek",
+    genre: { _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy" },
+    numberInStock: 6,
+    dailyRentalRate: 3.5
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd471828",
+    title: "Pulp Fiction",
+    genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller" },
+    numberInStock: 3,
+    dailyRentalRate: 4.0
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd471829",
+    title: "The Godfather",
+    genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller" },
+    numberInStock: 6,
+    dailyRentalRate: 4.5
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd47182a",
+    title: "The Matrix",
+    genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Action" },
+    numberInStock: 8,
+    dailyRentalRate: 3.5
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd47182b",
+    title: "Se7en",
+    genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller" },
+    numberInStock: 4,
+    dailyRentalRate: 3.5
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd47182c",
+    title: "Fight Club",
+    genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller" },
+    numberInStock: 5,
+    dailyRentalRate: 3.5
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd47182d",
+    title: "The Shawshank Redemption",
+    genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller" },
+    numberInStock: 7,
+    dailyRentalRate: 4.0
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd47182e",
+    title: "Jurassic Park",
+    genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Action" },
+    numberInStock: 6,
+    dailyRentalRate: 3.5
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd47182f",
+    title: "Toy Story",
+    genre: { _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy" },
+    numberInStock: 7,
+    dailyRentalRate: 3.0
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd471830",
+    title: "Finding Nemo",
+    genre: { _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy" },
+    numberInStock: 7,
+    dailyRentalRate: 3.0
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd471831",
+    title: "Titanic",
+    genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller" },
+    numberInStock: 5,
+    dailyRentalRate: 4.0
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd471832",
+    title: "Avatar",
+    genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Action" },
+    numberInStock: 6,
+    dailyRentalRate: 3.5
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd471833",
+    title: "Mad Max: Fury Road",
+    genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Action" },
+    numberInStock: 5,
+    dailyRentalRate: 3.5
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd471834",
+    title: "The Lion King",
+    genre: { _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy" },
+    numberInStock: 7,
+    dailyRentalRate: 3.0
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd471835",
+    title: "Braveheart",
+    genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Action" },
+    numberInStock: 4,
+    dailyRentalRate: 3.5
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd471836",
+    title: "The Silence of the Lambs",
+    genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller" },
+    numberInStock: 6,
+    dailyRentalRate: 4.0
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd471837",
+    title: "The Usual Suspects",
+    genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller" },
+    numberInStock: 4,
+    dailyRentalRate: 3.5
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd471838",
+    title: "Schindler's List",
+    genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller" },
+    numberInStock: 3,
+    dailyRentalRate: 4.5
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd471839",
+    title: "Goodfellas",
+    genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller" },
+    numberInStock: 6,
+    dailyRentalRate: 4.0
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd47183a",
+    title: "The Green Mile",
+    genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller" },
+    numberInStock: 7,
+    dailyRentalRate: 4.0
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd47183b",
+    title: "Saving Private Ryan",
+    genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Action" },
+    numberInStock: 5,
+    dailyRentalRate: 3.5
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd47183c",
+    title: "Inglourious Basterds",
+    genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Action" },
+    numberInStock: 4,
+    dailyRentalRate: 3.5
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd47183d",
+    title: "The Prestige",
+    genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller" },
+    numberInStock: 6,
+    dailyRentalRate: 3.5
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd47183e",
+    title: "Memento",
+    genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller" },
+    numberInStock: 5,
+    dailyRentalRate: 3.5
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd47183f",
+    title: "Spirited Away",
+    genre: { _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy" },
+    numberInStock: 7,
+    dailyRentalRate: 3.0
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd471840",
+    title: "The Departed",
+    genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller" },
+    numberInStock: 5,
+    dailyRentalRate: 3.5
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd471841",
+    title: "The Wolf of Wall Street",
+    genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller" },
+    numberInStock: 6,
+    dailyRentalRate: 4.0
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd471842",
+    title: "No Country for Old Men",
+    genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller" },
+    numberInStock: 4,
+    dailyRentalRate: 3.5
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd471843",
+    title: "The Great Escape",
+    genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Action" },
+    numberInStock: 3,
     dailyRentalRate: 3.5
   }
 ];
